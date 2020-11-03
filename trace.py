8da1362328e7bb3fceb24a4e816e89a4f819d3b4
Michael DeMarco <michaelfromyeg@gmail.com>
2020-11-03 11:13:56 -0800

    Initial commit

---
+++ trace.py
@@ -0,0 +1,29 @@
+# from typing import List, Enum
+
+def solve():
+    '''
+
+    '''
+    return 0
+
+def read(): 
+    '''
+    Read in the contract tracing 
+    '''
+    return 0
+
+def trace():
+    '''
+    Determine who is exposed to the virus
+    '''
+    return 0
+
+def contact():
+    '''
+    Contact those who are exposed to the virus
+    '''
+    return 0
+
+if __name__ == '__main__':
+    print("Hello, world!")
+    solve()
